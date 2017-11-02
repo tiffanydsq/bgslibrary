b48baa8716d286234eeb347d1b8b9dd26544faaa
tiffanydsq <tiffanydsq@gmail.com>
2017-11-02 17:03:47 -0400

    detect from images

--- Demo.cpp
+++ Demo.cpp
@@ -22,125 +22,140 @@ along with BGSLibrary.  If not, see <http://www.gnu.org/licenses/>.
 
 int main(int argc, char **argv)
 {
-  std::cout << "Using OpenCV " << CV_MAJOR_VERSION << "." << CV_MINOR_VERSION << "." << CV_SUBMINOR_VERSION << std::endl;
+    // VideoCapture capture;
+    if (argc < 3)
+    {
+        cout << "Not enough input arguments!" << endl;
+        return -1;
+    }
 
-  VideoCapture capture;
-  
+    // string input_name=argv[1];
+    // string suffix=input_name.substr(input_name.find_last_of('.'),input_name.size());
+    // cout<<"input_name: "<<input_name<<endl;
+    // if(inputname==".avi") // Video
+    // {
+    //       std::cout << "Openning: " << argv[1] << std::endl;
+    //       capture.open(argv[1]);
+    //       if (!capture.isOpened())
+    //       {
+    //         std::cerr << "Cannot initialize video!" << std::endl;
+    //         return -1;
+    //       }
+    // }
+    // else // image
+    // {
 
-  if (argc > 1)
-  {
-    std::cout << "Openning: " << argv[1] << std::endl;
-    capture.open(argv[1]);
-  }
-  else
-    capture.open(0);
+    vector<String> file_names;
+    glob(argv[1], file_names);
 
-  if (!capture.isOpened())
-  {
-    std::cerr << "Cannot initialize video!" << std::endl;
-    return -1;
-  }
+    if(!file_names.size()){
+        cout << "Empty folder / wrong path!" << endl;
+        return -1;
+    }    
+    // }
 
-  
-  VideoWriter video_mask(argv[1]+string("_fg.avi"),
-    CV_FOURCC('M','J','P','G'), 
-    capture.get(CV_CAP_PROP_FPS), 
-    Size(capture.get(CV_CAP_PROP_FRAME_WIDTH),capture.get(CV_CAP_PROP_FRAME_HEIGHT))
-  );
+    string output_name=argv[2];
+    cout<<"output_name: "<<output_name<<endl;
+    Mat frame=imread(file_names[0]);
+    VideoWriter video_mask(output_name,
+        CV_FOURCC('M','J','P','G'), 
+        5, 
+        Size(frame.cols,frame.rows)
+    );
 
-  /* Background Subtraction Methods */
-  IBGS *bgs;
+    IBGS *bgs;
+    bgs = new MultiLayer;
 
-  // bgs = new FrameDifference;
-  //bgs = new StaticFrameDifference;
-  //bgs = new WeightedMovingMean;
-  //bgs = new WeightedMovingVariance;
-  //bgs = new MixtureOfGaussianV1; // only on OpenCV 2.x
-  //bgs = new MixtureOfGaussianV2;
-  //bgs = new AdaptiveBackgroundLearning;
-  //bgs = new AdaptiveSelectiveBackgroundLearning;
-  //bgs = new GMG; // only on OpenCV 2.x
-  //bgs = new KNN; // only on OpenCV 3.x
-  //bgs = new DPAdaptiveMedian;
-  //bgs = new DPGrimsonGMM;
-  //bgs = new DPZivkovicAGMM;
-  //bgs = new DPMean;
-  //bgs = new DPWrenGA;
-  //bgs = new DPPratiMediod;
-  //bgs = new DPEigenbackground;
-  //bgs = new DPTexture;
-  //bgs = new T2FGMM_UM;
-  //bgs = new T2FGMM_UV;
-  //bgs = new T2FMRF_UM;
-  //bgs = new T2FMRF_UV;
-  //bgs = new FuzzySugenoIntegral;
-  //bgs = new FuzzyChoquetIntegral;
-  bgs = new MultiLayer;
-  //bgs = new PixelBasedAdaptiveSegmenter;
-  //bgs = new LBSimpleGaussian;
-  //bgs = new LBFuzzyGaussian;
-  //bgs = new LBMixtureOfGaussians;
-  //bgs = new LBAdaptiveSOM;
-  //bgs = new LBFuzzyAdaptiveSOM;
-  //bgs = new LBP_MRF;
-  //bgs = new VuMeter;
-  //bgs = new KDE;
-  //bgs = new IndependentMultimodal;
-  //bgs = new MultiCue;
-  //bgs = new SigmaDelta;
-  //bgs = new SuBSENSE;
-  //bgs = new LOBSTER;
-  //bgs = new PAWCS;
-  //bgs = new TwoPoints;
-  //bgs = new ViBe;
-  //bgs = new CodeBook;
-  
-  // namedWindow("Input", WINDOW_NORMAL );
-  // resizeWindow("Input",600, 800);
+    /* Background Subtraction Methods */
+/*
+    // bgs = new FrameDifference;
+    //bgs = new StaticFrameDifference;
+    //bgs = new WeightedMovingMean;
+    //bgs = new WeightedMovingVariance;
+    //bgs = new MixtureOfGaussianV1; // only on OpenCV 2.x
+    //bgs = new MixtureOfGaussianV2;
+    //bgs = new AdaptiveBackgroundLearning;
+    //bgs = new AdaptiveSelectiveBackgroundLearning;
+    //bgs = new GMG; // only on OpenCV 2.x
+    //bgs = new KNN; // only on OpenCV 3.x
+    //bgs = new DPAdaptiveMedian;
+    //bgs = new DPGrimsonGMM;
+    //bgs = new DPZivkovicAGMM;
+    //bgs = new DPMean;
+    //bgs = new DPWrenGA;
+    //bgs = new DPPratiMediod;
+    //bgs = new DPEigenbackground;
+    //bgs = new DPTexture;
+    //bgs = new T2FGMM_UM;
+    //bgs = new T2FGMM_UV;
+    //bgs = new T2FMRF_UM;
+    //bgs = new T2FMRF_UV;
+    //bgs = new FuzzySugenoIntegral;
+    //bgs = new FuzzyChoquetIntegral;
+    bgs = new MultiLayer;
+    //bgs = new PixelBasedAdaptiveSegmenter;
+    //bgs = new LBSimpleGaussian;
+    //bgs = new LBFuzzyGaussian;
+    //bgs = new LBMixtureOfGaussians;
+    //bgs = new LBAdaptiveSOM;
+    //bgs = new LBFuzzyAdaptiveSOM;
+    //bgs = new LBP_MRF;
+    //bgs = new VuMeter;
+    //bgs = new KDE;
+    //bgs = new IndependentMultimodal;
+    //bgs = new MultiCue;
+    //bgs = new SigmaDelta;
+    //bgs = new SuBSENSE;
+    //bgs = new LOBSTER;
+    //bgs = new PAWCS;
+    //bgs = new TwoPoints;
+    //bgs = new ViBe;
+    //bgs = new CodeBook;
+*/
 
-  // namedWindow("FG mask", cv::WINDOW_NORMAL );
-  // resizeWindow("FG mask",1200, 800);
+    // namedWindow("Input", WINDOW_NORMAL );
+    // resizeWindow("Input",600, 800);
 
-  int key = 0;
-  cv::Mat img_input;
-  int cnt=0;
-  while (key != 'q')
-  {
-    capture >> img_input;
-    if (img_input.empty()) break;
+    // namedWindow("FG mask", cv::WINDOW_NORMAL );
+    // resizeWindow("FG mask",1200, 800);
 
-    // cv::imshow("Input", img_input);
-    
+    int key = 0;
+    int cnt=0;
+    // while (key != 'q')
+    // {
+    for(string filename:file_names){
+        cv::Mat img_input;
+        cv::Mat img_mask;
+        cv::Mat img_bkgmodel;
 
-    cv::Mat img_mask;
-    cv::Mat img_bkgmodel;
+        img_input=imread(filename);
+        // capture >> img_input;
+        if (img_input.empty()) break;
+        cnt++;
+        // if(!(cnt%20))
+        cout<<"Frame No."<<cnt<<"   ";
 
-    bgs->process(img_input, img_mask, img_bkgmodel); // by default, it shows automatically the foreground mask image
-    //if(!img_mask.empty())
-    //  cv::imshow("FG mask", img_mask);
+        bgs->process(img_input, img_mask, img_bkgmodel); // by default, it shows automatically the foreground mask image
+        // cv::imshow("Input", img_input);
+        //if(!img_mask.empty())
+        //  cv::imshow("FG mask", img_mask);
 
-    cv::Mat white;
-    findNonZero	(img_mask,white);
-    cout<< white.cols << "  " << white.rows << endl;
+        cv::Mat white;
+        findNonZero	(img_mask,white);
+        cout<<"Number of foreground pixels: " << white.rows << endl;
 
-    //  process 
-    cv::Mat mask;
-    cvtColor(img_mask, mask, cv::COLOR_GRAY2BGR);
-    video_mask<<mask;
+        //  save mask 
+        cv::Mat mask;
+        cvtColor(img_mask, mask, cv::COLOR_GRAY2BGR);
+        video_mask<<mask;
 
+        key = cvWaitKey(33);
+    }
 
+    delete bgs;
 
-    cnt++;
-    if(!(cnt%50))
-      cout<<"Frame No."<<cnt<<endl;
+    // capture.release();
+    cvDestroyAllWindows();
 
-    key = cvWaitKey(33);
-  }
-
-  delete bgs;
-
-  capture.release();
-  cvDestroyAllWindows();
-
-  return 0;
-}
+    return 0;
+    }
